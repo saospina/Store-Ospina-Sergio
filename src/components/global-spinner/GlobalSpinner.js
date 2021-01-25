c0e50558bdaad85df416d3f8389872b5983953d6
saospina <sergos0617@gmail.com>
2021-01-24 22:43:23 -0500

    adding spinner css styles

--- src/components/global-spinner/GlobalSpinner.js
+++ src/components/global-spinner/GlobalSpinner.js
@@ -3,11 +3,20 @@ import React from 'react'
 import './GlobalSpinner.css';
 
 const GlobalSpinner = ({ spinner }) => {
+
+    const spinnerStyle = {
+        width: '3rem',
+        height: '3rem'
+    }
     if (!spinner) return null;
     return (
-        <div className="spinner">
-            Loading...
+        <div className="spinner text-center text-info">
+            <div className="spinner-border" style={spinnerStyle} role="status">
+            </div>
+            <div className="spinner-grow" style={spinnerStyle} role="status">
+            </div>
         </div>
+
     )
 };
 
